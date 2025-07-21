219e41e1e72c84c0858f2d9e8c2739079f84a58f
Imetnowa <akwolagaawontemi@gmail.com>
2025-07-21 03:00:23 -0700

    OOP

---
+++ src/OOPpractice.java
@@ -0,0 +1,12 @@
+import HelloWorld.User;
+
+public class OOPpractice {
+    public static void main(String[] args){
+        System.out.println("Helloo World");
+        User u = new User();
+
+        u.name = "Caleb";
+        System.out.println(u.name);
+
+    }
+}
